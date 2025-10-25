e1bcf64d2c4d95aeab1fca8eda533644191fda19
gaberashko <gabrielrashko02@gmail.com>
2025-10-24 22:44:33 -0400

    Set up TypeScript, SCSS, and webpack workflow

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import "./styles/main.scss";
+
+const heading = document.createElement("h1");
+heading.textContent = "Hello!";
+document.body.appendChild(heading);
+
+console.log("It works!");
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,24 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     mode: "development",
-    entry: "./src/index.js",
+    entry: "./src/index.ts",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
         clean: true,
     },
-    plugins: [new HtmlWebpackPlugin({ template: "./src/template.html" })],
+    plugins: [
+        new HtmlWebpackPlugin({ template: "./src/index.html" }),
+        new MiniCssExtractPlugin(),
+    ],
     module: {
         rules: [
             {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
+                test: /\.scss$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
             },
             { test: /\.html$/i, loader: "html-loader" },
             {
@@ -25,7 +29,15 @@ module.exports = {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
                 type: "asset/resource",
             },
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
         ],
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
     },
     devtool: "eval-source-map",
     devServer: {
